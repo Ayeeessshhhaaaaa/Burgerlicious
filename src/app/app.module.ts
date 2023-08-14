93a60a7cc46c0f344b68276cc6607570f2f380ce
ShafwanAnwer <shafwan.anwer@gmail.com>
2023-08-14 16:06:04 +0530

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TestComponent } from './Components/test/test.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -14,7 +13,8 @@ import { TestComponent } from './Components/test/test.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    FlexLayoutModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
