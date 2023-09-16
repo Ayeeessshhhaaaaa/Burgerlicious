fdb863ae6cf2f7b018994a1b00419c6bcc93d055
Ayeeessshhhaaaaa <ayeshafaz90@gmail.com>
2023-09-16 15:37:46 +0530

    splashpage

--- src/app/Pages/splash-screen/splash-screen.component.ts
+++ src/app/Pages/splash-screen/splash-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-splash-screen',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./splash-screen.component.scss']
 })
 export class SplashScreenComponent {
+
+  imageUrl: string = 'assets/splash-image.png';
+  sanitizedImageUrl: SafeUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.sanitizedImageUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.imageUrl);
+  }
 
 }
